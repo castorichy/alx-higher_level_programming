4f8a095621cde59b75ba14938e12862dfb627b90
castorichy <castorichy@gmail.com>
2022-11-29 14:51:54 +0300

    loops and if else statements solved

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = int(repr(number)[-1])
+if number < 0:
+    last_digit = -(last_digit)
 str = f"Last digit of {number} is {last_digit} and is"
 if last_digit > 5:
     print(f'{str:s} greater than 5')
@@ -9,3 +11,5 @@ elif last_digit == 0:
     print(f"{str:s} 0")
 elif (last_digit < 6) and (last_digit != 0):
     print(f"{str:s} less than 6 and not 0")
+else:
+    print("ValueError")
