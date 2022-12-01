8efe5860f693faf0cd6bc741b250ae1529136c6c
castorichy <castorichy@gmail.com>
2022-12-01 13:29:53 +0300

    calculator program

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import calculator_1
+
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    print("{} + {} = {}".fomat(a, b, add(a, b)))
+    print("{} - {} = {}".fomat(a, b, sub(a, b)))
+    print("{} * {} = {}".fomat(a, b, mul(a, b)))
+    print("{} / {} = {}".fomat(a, b, div(a, b)))
