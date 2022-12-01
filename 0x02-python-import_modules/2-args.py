13106d134e754105b3b284d0c7435217dfc18961
castorichy <castorichy@gmail.com>
2022-12-01 14:17:54 +0300

    multi space removed

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+
+
+def com_args():
+    if len(sys.argv[1:]) < 1:
+        print("{} arguments.".format(len(sys.argv[1:])))
+    elif len(sys.argv[1:]) == 1:
+        print("{} argument:".format(len(sys.argv[1:])))
+    else:
+        print("{} arguments:".format(len(sys.argv[1:])))
+    for i in range(1, len(sys.argv[1:]) + 1):
+        print("{}: {}".format(i, sys.argv[i]))
+
+
+com_args()
