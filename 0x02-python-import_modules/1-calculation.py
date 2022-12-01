c2f36c4acda28990142ab014c117bd16441a1828
castorichy <castorichy@gmail.com>
2022-12-01 13:35:14 +0300

    calculator program

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+import calculator_1 as cal
 
 if __name__ == '__main__':
     a = 10
     b = 5
-    print("{} + {} = {}".fomat(a, b, add(a, b)))
-    print("{} - {} = {}".fomat(a, b, sub(a, b)))
-    print("{} * {} = {}".fomat(a, b, mul(a, b)))
-    print("{} / {} = {}".fomat(a, b, div(a, b)))
+    print("{} + {} = {}".fomat(a, b, cal.add(a, b)))
+    print("{} - {} = {}".fomat(a, b, cal.sub(a, b)))
+    print("{} * {} = {}".fomat(a, b, cal.mul(a, b)))
+    print("{} / {} = {}".fomat(a, b, cal.div(a, b)))
