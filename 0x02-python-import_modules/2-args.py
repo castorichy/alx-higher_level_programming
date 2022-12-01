1acc1d6ce77c0cdcea4159f79ea0840a42aa64a7
castorichy <castorichy@gmail.com>
2022-12-01 14:27:40 +0300

    multi space removed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-import sys
+from sys import argv
 
 if __name__ == '__main__':
-    if len(sys.argv[1:]) < 1:
-        print("{} arguments.".format(len(sys.argv[1:])))
-    elif len(sys.argv[1:]) == 1:
-        print("{} argument:".format(len(sys.argv[1:])))
+    if len(argv[1:]) < 1:
+        print("{} arguments.".format(len(argv[1:])))
+    elif len(argv[1:]) == 1:
+        print("{} argument:".format(len(argv[1:])))
     else:
-        print("{} arguments:".format(len(sys.argv[1:])))
-    for i in range(1, len(sys.argv[1:]) + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(len(argv[1:])))
+        for i in range(1, len(argv[1:]) + 1):
+            print("{}: {}".format(i, argv[i]))
