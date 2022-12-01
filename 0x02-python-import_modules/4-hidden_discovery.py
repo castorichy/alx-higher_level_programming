c33627a1911bb58c3ca5c5b2aa18f268554b8fb7
castorichy <castorichy@gmail.com>
2022-12-01 17:41:39 +0300

    intcluding str.format

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4 
-    for item in dir(hidden_4):
+    import hidden_4 as hid
+    for item in dir(hid):
         if "_" not in item:
             print(item)
