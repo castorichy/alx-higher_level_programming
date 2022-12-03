537d577da595f9dff2f0434d95d332b7549533a0
castorichy <castorichy@gmail.com>
2022-12-03 12:14:04 +0300

    not executable when imported

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{}".format(num))
-
-
-print_list_integer([3, 4, 5, 7, 2, 33])
-if __name__ == "__main__":
-    print_list_integer(my_list=[])
+if __name__ = "__main__":
+    def print_list_integer(my_list=[]):
+        for num in my_list:
+            print("{}".format(num))
