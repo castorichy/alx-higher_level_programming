cfc0d843ecf1f9ec33ad4720aa063cf426490e9f
castorichy <castorichy@gmail.com>
2022-12-03 12:53:59 +0300

    main in name updated

--- 0x03-python-data_structures/testList.py
+++ 0x03-python-data_structures/testList.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     element_at = __import__ ("1-element_at").element_at
     my = [12, 45, 65, 66, 5, 44, 33, 3]
-    idx = 4
+    idx = 8
     print(element_at(my, idx))
