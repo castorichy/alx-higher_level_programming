03f3c9036b8c2b889454c99f65b06428840969e8
castorichy <castorichy@gmail.com>
2022-12-03 13:19:12 +0300

    main in name updated

--- 0x03-python-data_structures/testList.py
+++ 0x03-python-data_structures/testList.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    element_at = __import__ ("2-replace_in_list").replace_in_list
-    my = [12, 45, 65, 66, 5, 44, 33, 3]
+    element_at = __import__ ("3-print_reversed_list_integer").print_reversed_list_integer
+    my = [9, 12, 45, 65, 66, 5, 44, 33, 3]
     idx = -1
     elem = 44
-    print(element_at(my, idx, elem))
+    print(element_at(my))
