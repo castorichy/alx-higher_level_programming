750c1637a4142a04b8e0d7735208131b47903933
castorichy <castorichy@gmail.com>
2022-12-03 12:37:02 +0300

    main in name updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx > 0 and idx <= len(my_list):
-        print("{}".format(my_list[idx]))
+        return my_list[idx]
 
 
 if __name__ == "__main__":
