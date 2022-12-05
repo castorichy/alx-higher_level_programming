b4660e6b68440b8b41dc6b81879860f7350d300b
castorichy <castorichy@gmail.com>
2022-12-05 12:00:23 +0300

    get tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,12 +2,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = [0, 0]
     b = [0, 0]
-    for index, value in enumerate(tuple_a):
+    for index, i in enumerate(tuple_a):
         if index < 2:
-            a[index] = value[index]
-    for index, value in enumerate(tuple_b):
+            a[index] = tuple_a[index]
+    for index, i in enumerate(tuple_b):
         if index < 2:
-            b[index] = value[index]
-    sum_a = a[1] + b[1]
-    sum_b = a[2] + b[2]
-    return sum_a, sum_b
+            b[index] = tuple_b[index]
+    sum_a = a[0] + b[0]
+    sum_b = a[1] + b[1]
+    return (sum_a, sum_b)
