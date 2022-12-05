634d80da658999a4eb208102135650560002ba1e
castorichy <castorichy@gmail.com>
2022-12-05 11:18:12 +0300

    get matrix in list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        for i in range(len(matrix)):
-            for lst in matrix[i]:
+        for row in matrix:
+            for index, lst in row:
                 print("{:d}".format(lst), end=' ')
-            print()
+                if index < len(row):
+                    print("{}".format(" "), end='')
