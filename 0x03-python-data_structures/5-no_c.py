2fdebce2761d947d130255faf20bfaafa69e41b4
castorichy <castorichy@gmail.com>
2022-12-05 10:44:14 +0300

    remove c in a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
+def no_c(my_string):
+    for letter in my_string:
+        if letter == 'c' or letter == 'C':
+            print(end='')
+        else:
+            print(letter, end='')
+
+
 if __name__ == "__main__":
-    def no_c(my_string):
-        for letter in  my_string:
-            if letter != 'c' or letter != 'C':
-                print(letter, end='')
-            else:
-                print(end='')
-no_c("can Copy")
+    no_c()
