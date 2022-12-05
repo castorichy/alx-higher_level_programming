01021b7881365c6422a7ff24bc64caa134051601
castorichy <castorichy@gmail.com>
2022-12-05 10:22:41 +0300

    repace all

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = []
-    if idx >= 0 and idx < len(new_list):
-        new_list.append(my_list[:])
-        new_list[idx] = element
-        return new_list
-    else:
-        return my_list
+    if my_list:
+        if idx >= 0 and idx < len(my_list):
+            new_list = []
+            for num in my_list:
+                new_list.append(num)
+            for i in range(len(my_list) - 1):
+                new_list[idx] = element
+                return new_list
+        else:
+            return my_list
 
 
-if __name__ == "__main__":
-    new_in_list()
+lis = [12, 33, 22, 44]
+idx = 3
+el = 66
+print(new_in_list(lis, idx, el))
+print(lis)
+'''if __name__ == "__main__":
+    new_in_list()'''
