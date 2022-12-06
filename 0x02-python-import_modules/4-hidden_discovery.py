86728c75a2542914962716f1f50b716c013b3483
castorichy <castorichy@gmail.com>
2022-12-06 14:06:20 +0300

    editted

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import hidden_4 as hid
-    for item in dir(hid):
-        if '_' not in item:
-            print(item)
+    import hidden_4
+    for i in dir(hidden_4):
+        if i.startswith("__"):
+            continue
+        print(i)
