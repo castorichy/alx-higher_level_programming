457c9ca18671465d78b0756e2d999fbdef498aa2
castorichy <castorichy@gmail.com>
2022-12-07 22:17:59 +0300

    common elements in sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if set_1 and set_2:
-        lis = list(set_1 & set_2)
-        if not lis:
-            return None
-        else:
-            return lis
+    if set_1:
+        if set_2:
+            lis = list(set_1 & set_2)
+            if not lis:
+                return None
+            else:
+                return lis
 
 
 if __name__ == "__main__":
