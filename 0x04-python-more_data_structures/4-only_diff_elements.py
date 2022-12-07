0d9b33ffd256782d1c64d3c9a486b3eb734904ed
castorichy <castorichy@gmail.com>
2022-12-07 22:48:59 +0300

    diffrent elements in sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
     if set_1:
         if set_2:
             lis = list(set_1 - set_2)
@@ -8,4 +8,4 @@ def common_elements(set_1, set_2):
 
 
 if __name__ == "__main__":
-    common_elements()
+    only_diff_element()
