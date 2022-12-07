1f2efde7d405a1e6f0df3607787b04f72dce4ddc
castorichy <castorichy@gmail.com>
2022-12-07 22:45:40 +0300

    diffrent elements in sets

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    if set_1:
+        if set_2:
+            lis = list(set_1 - set_2)
+            if lis is not None:
+                return lis
+
+
+if __name__ == "__main__":
+    common_elements()
