fed65204a8ba0dbd6ce1cac428193dd1d09cfa5f
castorichy <castorichy@gmail.com>
2022-12-19 23:32:18 +0300

    Value Error exception in list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for idx in range(x):
         try:
             print("{:d}".format(my_list[idx]), end="")
+            y += 1
         except (ValueError, TypeError):
-            y -= 1
             continue
     print()
-    return (idx + 1) + y
+    return y
