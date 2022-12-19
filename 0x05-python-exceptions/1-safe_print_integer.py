e0c7391384c2649c37e3cb3b9608b1ea71c689b4
castorichy <castorichy@gmail.com>
2022-12-19 22:53:25 +0300

    ValueError Exeption solved

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if value:
-        try:
-            print("{:d}".format(value))
-        except (ValueError, TypeError):
-            return False
-        else:
-            return True
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError):
+        return False
+    else:
+        return True
