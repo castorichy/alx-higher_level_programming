c84a8e1a720c1f29453db089454c10afc96f9496
castorichy <castorichy@gmail.com>
2022-12-19 17:28:05 +0300

    IndexError Exeption solved

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,10 +6,8 @@ def safe_print_list(my_list=[], x=0):
             for idx in range(x):
                 print("{}".format(my_list[idx]), end="")
                 y += 1
-            print()
             return y
         except IndexError:
-            print()
             return y
 
 
